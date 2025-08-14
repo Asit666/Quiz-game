70f761746f32ed0534f9cf40c1b5d40b3cce4265
Asit666 <abhisekmohapatra.2013@gmail.com>
2025-08-14 15:58:18 +0530

    Initial commit
    looks good

---
+++ QuizGame.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class QuizGame {
+    
+    public static void main(String[] args) {
+        Scanner A = new Scanner(System.in);
+        System.out.println("-----------------------welcome-----------------------");
+
+        String[] questions = {"what is the capital of France?", "which language is used for Android development?","who developed Java"};
+
+        String[][] options = {{"A. Paris","B. Berlin","C. Madria","D. Rome"} ,{"A. Swift", "B.Kotlin", "C. JavaScript","D. Py"}};
+        
+    }
+}
